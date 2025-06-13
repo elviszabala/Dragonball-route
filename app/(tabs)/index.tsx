a5e6073e3fccf0a36b43acaaaa9f0db56cab929c
Elvis <zabalaelvis@hotmail.com>
2025-06-13 11:17:15 +0200

    Cambios
    nuevos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { ActivityIndicator, StyleSheet, FlatList, Image } from 'react-native';
+import { ActivityIndicator, StyleSheet, FlatList, Image, SafeAreaView, Dimensions, useWindowDimensions } from 'react-native';
 import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { GetAllData } from '../../lib/dragonball';
 import { Characters, Item } from '../../interfaces/Characters';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Character } from '../../interfaces/Character';
 
 
 
@@ -12,6 +13,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 export default function TabOneScreen() {
 
   const [data, setData] = useState<Characters | null>(null);
+  const {width, height} = useWindowDimensions();
 
   useEffect(() => {
 
@@ -22,15 +24,43 @@ export default function TabOneScreen() {
    
   }, []);
   //const itemsArray = data?.items ? Object.values(data.items) : [];
-
+  const Carousel = ({ data }: { data: Character[] }) => (
+    <View style={styles.carouselSection}>
+      
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <ShowCardD item={item} />}
+        keyExtractor={item => item.id.toString()}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.carouselList}
+      />
+    </View>
+  );
+  const ShowCardD = ({ item }: { item: Character }) => (
+    <View style={{ ...styles.cardContainer, width: width * 0.9 }}>
+      
+      <Image source={{ uri: item.image }} style={styles.cardImage} />
+      <Text style={styles.textContainer} >
+        <Text style={styles.cardTitle} numberOfLines={1}>
+          {"Name: " + item.name}
+        </Text>
+      </Text>
+    </View>
+  );
 
   return (
+    <SafeAreaView style={styles.container}>
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>Dragon Ball Characters</Text>
+      </View>
     <View style={styles.container}>
       { data?.items === null ? (
         //<ActivityIndicator size="large" color="#0000ff" />
         <Text>Loading...</Text>
       ) : (
-       <FlatList
+
+        <Carousel data={data?.items as Character[] ?? []} />
+    /*    <FlatList
         data={data?.items}
         keyExtractor={(item: Item) => item.id.toString()}
         renderItem={({ item }) => (
@@ -44,12 +74,13 @@ export default function TabOneScreen() {
             </View>
         )}
 
-      />
+      /> */
 
       ) }
       
 
     </View>
+  </SafeAreaView>
   );
 }
 
@@ -73,6 +104,7 @@ const styles = StyleSheet.create({
   },
   carouselSection: {
     marginBottom: 24,
+   
   },
   carouselTitle: {
     fontSize: 20,
@@ -83,21 +115,34 @@ const styles = StyleSheet.create({
   },
   carouselList: {
     paddingHorizontal: 16,
+
+    
   },
   cardContainer: {
-    width: 150,
+    width: 150, // Adjust width based on screen size
     marginRight: 12,
+    //backgroundColor: 'pink',
+    padding: 8,
+    flexDirection: 'row',
+    //alignItems: 'center',
+
   },
   cardImage: {
-    width: '100%',
+    width: 150,
     height: 225,
     borderRadius: 4,
     backgroundColor: '#333',
+    resizeMode: 'contain',
   },
+  textContainer: {
+    flex: 1,
+    marginLeft: 12, // spacing between image and text
+},
   cardTitle: {
     color: '#ffffff',
     fontSize: 14,
     marginTop: 8,
+    flexDirection: 'row',
   },
   // --- Details Screen Styles ---
   detailsImage: {
